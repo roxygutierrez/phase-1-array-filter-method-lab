0fcdab22d5854932ec6b6aef985f15b1a095ba64
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-11-13 20:03:20 -0600

    commit tree 2954cabd4cde3d5ed8c173b5f24ec66619205225

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(arrayOfDrivers, driverName) {
+  return arrayOfDrivers.filter(function (driver) {
+    return driver.toUpperCase() === driverName.toUpperCase();
+  });
+}
+
+function fuzzyMatch(arrayOfDrivers, lookUp) {
+  return arrayOfDrivers.filter(function (name) {
+    return name.startsWith(lookUp);
+  });
+}
+
+function matchName(arrayOfDrivers, name) {
+  return arrayOfDrivers.filter(function (driver) {
+    return driver.name === name;
+  });
+}
